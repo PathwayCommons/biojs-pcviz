3901404ce4564042f8ade1e71d21bce3dd5db1d5
Xavier Watkins <xavier.watkins@gmail.com>
2015-03-09 14:54:56 +0000

    Changes to structure

--- snippets/simple_example.js
+++ snippets/simple_example.js
@@ -1,6 +1,6 @@
 // if you don't specify a html file, the sniper will generate a div
 
 var biojspcviz = require("biojs-pcviz");
-var pcviz = new biojspcviz();
+var pcviz = new biojspcviz({ el: yourDiv, query: "BRCA1"});
 // Visualize the neighborhood of "BRCA1" gene in a div using this new instance
-pcviz.neighborhood({ el: yourDiv, query: "BRCA1"});
+pcviz.neighborhood();
